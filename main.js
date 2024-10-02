1f34b6c977d154fe2314280912988156c1d824d3
radriann21 <adrianrangelargu@gmail.com>
2024-10-01 22:22:11 -0400

    added basic checking logic

--- main.js
+++ main.js
@@ -4,20 +4,45 @@ const quizContainer = document.getElementById('answersContainer')
 const question = document.getElementById('question')
 const check = document.getElementById('checkButton')
 let questionIndex = 0
+let answerIndex = null
 
-question.innerText = questions[questionIndex].question
+function initializeUI() {
+  question.innerText = questions[questionIndex].question
+}
 
-const answers = questions[questionIndex].answers 
-answers.forEach((answer, index) => {
+function setAnswer(evt) {
+  answerIndex = parseInt(evt.currentTarget.dataset.index) 
+}
+
+function createAnswers() {
+  const answers = questions[questionIndex].answers 
+  answers.forEach((answer, index) => {
   const answerElement = document.createElement('article')
   answerElement.setAttribute('id', 'question')
   answerElement.setAttribute('data-index', index)
+  answerElement.addEventListener('click', setAnswer)
+
   const answerText = document.createElement('p')
   answerText.innerText = answer
   answerElement.appendChild(answerText)
+
   quizContainer.appendChild(answerElement)
 })
+}
 
+function validateAnswer() {
+  if (questions[questionIndex].correct_answer === answerIndex) {
+    alert('correct!')
+  } else {
+    alert('incorrect!')
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initializeUI()
+  createAnswers()
+  check.addEventListener('click', validateAnswer)
+})
 
 
 
